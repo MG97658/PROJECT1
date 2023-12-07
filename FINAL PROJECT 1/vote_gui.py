f03e36ecc3a93d0b334b2976247908da00705b14
MG97658 <mauricegreen971@gmail.com>
2023-12-06 22:26:29 -0600

    made changes

--- FINAL PROJECT 1/vote_gui.py
+++ FINAL PROJECT 1/vote_gui.py
@@ -1,10 +1,20 @@
-# voting_gui.py
 import tkinter as tk
 from tkinter import messagebox
 from voting_functions import VotingAppFunctions
+from typing import Any
 
 class VotingAppGUI:
-    def __init__(self, master: tk.Tk) -> None:
+    """
+    Class for the graphical user interface of the voting app.
+    """
+    def __init__(self, master: tk.Tk, voting_functions: VotingAppFunctions) -> None:
+        """
+        Initialize the VotingAppGUI.
+
+        Parameters:
+            master (tk.Tk): The root Tkinter window.
+            voting_functions (VotingAppFunctions): Instance of VotingAppFunctions for handling voting logic.
+        """
         self.master = master
         self.master.title("Voting App")
         self.master.geometry("400x300")
@@ -12,11 +22,14 @@ class VotingAppGUI:
         # Disable resizing
         self.master.resizable(False, False)
 
-        self.voting_functions = VotingAppFunctions()
+        self.voting_functions = voting_functions
 
         self.create_widgets()
 
     def create_widgets(self) -> None:
+        """
+        Create the GUI widgets for the VotingApp.
+        """
         self.name_label = tk.Label(self.master, text='Your Name:')
         self.name_label.grid(row=0, column=0, pady=5, sticky='e')
 
@@ -28,37 +41,46 @@ class VotingAppGUI:
         self.candidate_menu_label = tk.Label(self.master, text='CANDIDATE MENU')
         self.candidate_menu_label.grid(row=1, column=0, columnspan=2, pady=10, sticky='n')
 
-        self.button1 = tk.Button(self.master, text='Vote for John', command=lambda: self.voting_functions.vote(1, self.name_entry))
-        self.button1.grid(row=2, column=0, padx=10, pady=5, sticky='e')
-
-        self.button2 = tk.Button(self.master, text='Vote for Jane', command=lambda: self.voting_functions.vote(2, self.name_entry))
-        self.button2.grid(row=2, column=1, padx=10, pady=5, sticky='w')
-
-        self.button3 = tk.Button(self.master, text='Vote for Jack', command=lambda: self.voting_functions.vote(3, self.name_entry))
-        self.button3.grid(row=3, column=0, padx=10, pady=5, sticky='e')
-
-        self.button4 = tk.Button(self.master, text='Vote for Jill', command=lambda: self.voting_functions.vote(4, self.name_entry))
-        self.button4.grid(row=3, column=1, padx=10, pady=5, sticky='w')
+        self.create_vote_button(1, 'John')
+        self.create_vote_button(2, 'Jane')
+        self.create_vote_button(3, 'Jack')
+        self.create_vote_button(4, 'Jill')
 
         self.exit_button = tk.Button(self.master, text='Exit', command=self.master.destroy)
-        self.exit_button.grid(row=4, column=0, columnspan=2, pady=10, sticky='s')
+        self.exit_button.grid(row=5, column=0, columnspan=2, pady=10, sticky='s')
 
         # Configure columns and rows to center content
-        self.master.columnconfigure(0, weight=1)
-        self.master.columnconfigure(1, weight=1)
-        self.master.rowconfigure(0, weight=1)
-        self.master.rowconfigure(1, weight=1)
-        self.master.rowconfigure(2, weight=1)
-        self.master.rowconfigure(3, weight=1)
-        self.master.rowconfigure(4, weight=1)
+        for i in range(6):
+            self.master.columnconfigure(i, weight=1)
+        for i in range(6):
+            self.master.rowconfigure(i, weight=1)
+
+    def create_vote_button(self, candidate: int, candidate_name: str) -> None:
+        """
+        Create a vote button for a candidate.
+
+        Parameters:
+            candidate (int): The candidate number.
+            candidate_name (str): The candidate's name.
+        """
+        button = tk.Button(self.master, text=f'Vote for {candidate_name}', command=lambda: self.voting_functions.vote(candidate, self.name_entry))
+        button.grid(row=candidate + 1, column=candidate % 2, padx=10, pady=5, sticky=('e', 'w'))
 
 def main() -> None:
+    """
+    Run the main VotingApp program.
+    """
     try:
         root = tk.Tk()
-        app = VotingAppGUI(root)
+        voting_functions = VotingAppFunctions()
+        app = VotingAppGUI(root, voting_functions)
         root.mainloop()
+
+        # After the GUI exits, you can access the vote results
+        print("Vote Results:", voting_functions.get_candidate_votes())
     except Exception as e:
         messagebox.showerror("Error", f"An unexpected error occurred: {str(e)}")
 
 if __name__ == '__main__':
     main()
+
--- FINAL PROJECT 1/voting_functions.py
+++ FINAL PROJECT 1/voting_functions.py
@@ -1,21 +1,41 @@
 # voting_functions.py
 import tkinter as tk
 from tkinter import messagebox
+from typing import Tuple
 
 class VotingAppFunctions:
+    """
+    Class for handling voting-related functions.
+    """
     def __init__(self):
-        self.can1_votes = 0
-        self.can2_votes = 0
-        self.can3_votes = 0
-        self.can4_votes = 0
+        """
+        Initialize the VotingAppFunctions.
+        """
+        self.can_votes = [0, 0, 0, 0]
 
     def validate_name(self, name: str) -> bool:
+        """
+        Validate the entered name.
+
+        Parameters:
+            name (str): The entered name.
+
+        Returns:
+            bool: True if the name is valid, False otherwise.
+        """
         if any(char.isdigit() for char in name):
             messagebox.showwarning("Invalid Name", "Name cannot contain numeric characters.")
             return False
         return True
 
     def vote(self, candidate: int, name_entry: tk.Entry) -> None:
+        """
+        Register a vote for the specified candidate and display a message.
+
+        Parameters:
+            candidate (int): The candidate for whom the vote is registered.
+            name_entry (tk.Entry): The Entry widget for the voter's name.
+        """
         try:
             voter_name = name_entry.get()
 
@@ -26,49 +46,22 @@ class VotingAppFunctions:
                 messagebox.showwarning("Missing Name", "Please enter your name before voting.")
                 return
 
-            if candidate == 1:
-                messagebox.showinfo("Vote", f'{voter_name} voted for John')
-                self.can1_votes += 1
-            elif candidate == 2:
-                messagebox.showinfo("Vote", f'{voter_name} voted for Jane')
-                self.can2_votes += 1
-            elif candidate == 3:
-                messagebox.showinfo("Vote", f'{voter_name} voted for Jack')
-                self.can3_votes += 1
-            elif candidate == 4:
-                messagebox.showinfo("Vote", f'{voter_name} voted for Jill')
-                self.can4_votes += 1
+            self.can_votes[candidate - 1] += 1
+
+            # Display a message indicating for whom the user voted
+            candidate_names = ['John', 'Jane', 'Jack', 'Jill']
+            messagebox.showinfo("Vote", f"{voter_name} voted for {candidate_names[candidate - 1]}")
 
             # Clear the content of the name entry
             name_entry.delete(0, tk.END)
         except Exception as e:
             messagebox.showerror("Error", f"An unexpected error occurred: {str(e)}")
 
-    def candidate_menu(self) -> int:
-        while True:
-            try:
-                print('----------------------\nCANDIDATE MENU\n----------------------')
-                print('1 - John')
-                print('2 - Jane')
-                print('3 - Jack')
-                print('4 - Jill')
-                choice = int(input('Candidate: '))
-                if 1 <= choice <= 4:
-                    return choice
-                else:
-                    print('Invalid choice. Please enter a number between 1 and 4.')
-            except ValueError:
-                print('Invalid input. Please enter a valid integer (1, 2, 3, or 4).')
+    def get_candidate_votes(self) -> Tuple[int, int, int, int]:
+        """
+        Get the total votes for each candidate.
 
-    def vote_menu(self) -> str:
-        while True:
-            try:
-                print('---------\nVOTE MENU\n---------')
-                print('v: VOTE\nx: Exit')
-                option = input('Option: ').strip().lower()
-                if option in ('v', 'x'):
-                    return option
-                else:
-                    print('Invalid option. Please enter either "v" for vote or "x" for exit.')
-            except Exception:
-                print('An unexpected error occurred.')
+        Returns:
+            Tuple[int, int, int, int]: Votes for each candidate.
+        """
+        return tuple(self.can_votes)
